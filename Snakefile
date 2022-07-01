d6470870c1a606eb2e542f1c5c1ac50f562a3771
tischulz1 <tischulz@cebitec.uni-bielefeld.de>
2022-07-01 14:39:35 +0200

    Adapted and extended notebook and workflow. Also changed sequence simulation script to make it use a randomly chosen seed by default

--- Snakefile
+++ Snakefile
@@ -1,27 +1,83 @@
 configfile: 'config.yaml'
 
+NB_RANDSEQS=(config['nbCpys'] + 1) * config['nbSimSeqs']
+
 rule all:
 	input:
-		expand("../simulations/scores_mes{mes}_s{s}_n{n}_l{l}_m{m}_i{m}_d{m}.txt", mes=config['simMeasure'], s=config['randomSeed'], n=\
-			config['nbSimSeqs'], l=config['simSeqLen'], m=config['mutationRates'])
+		expand("../simulations/homologies_gn{gn}_rn{rn}_gl{gl}_rl{rl}_o{o}_m{m}_i{m}_d{m}.txt", gn=config['nbSimSeqs'], rn=NB_RANDSEQS, gl=\
+			config['geneLen'], rl=config['randSeqLen'], o=config['nbCpys'], m=config['mutationRates'] + [0])#,
+		#expand("../simulations/scores_mes{mes}_n{n}_l{l}_m{m}_i{m}_d{m}.txt", mes=config['simMeasure'], n=\
+		#	config['nbSimSeqs'], l=config['simSeqLen'], m=config['mutationRates'])
 
 rule simSeqs:
 	output:
-		"../simulations/simSeqs_s{s}_n{n}_l{l}_m{m}_i{i}_d{d}.ssv"
+		"../simulations/simSeqs_n{n}_l{l}_m{m}_i{i}_d{d}.ssv"
 	shell:
-		"python3 SimSeqPairs.py -s {wildcards.s} -n {wildcards.n} -l {wildcards.l} -m {wildcards.m} -i {wildcards.i} -d {wildcards.d} > {output}"
+		"python3 scripts/SimSeqPairs.py -n {wildcards.n} -l {wildcards.l} -m {wildcards.m} -i {wildcards.i} -d {wildcards.d} > {output}"
 
 rule measureSimilarity:
 	input:
-		"../simulations/simSeqs_s{simDesc}.ssv"
+		"../simulations/simSeqs_n{simDesc}.ssv"
 	params:
 		simMes = "{mes}"
 	output:
-		"../simulations/scores_mes{mes}_s{simDesc}.txt"
+		"../simulations/scores_mes{mes}_n{simDesc}.txt"
 	run:
 		for p in open(input[0], 'r'):
-			seqs = p.strip().split(' ')
-			if params.simMes == "intersec":
-				shell("src/CalcSim -i -a {seqs[0]} -b {seqs[1]} >> {output}")
-			else:
-				shell("src/CalcSim -l -a {seqs[0]} -b {seqs[1]} >> {output}")
+			if not p.startswith("Random"):
+				seqs = p.strip().split(' ')
+				if params.simMes == "intersec":
+					shell("src/CalcSim -i -a {seqs[0]} -b {seqs[1]} >> {output}")
+				else:
+					shell("src/CalcSim -l -a {seqs[0]} -b {seqs[1]} >> {output}")
+
+rule simGeneSeqs:
+	output:
+		"../simulations/simGeneSeqs_n{n}_l{l}_o{o}_m{m}_i{i}_d{d}.ssv"
+	shell:
+		"python3 scripts/SimSeqPairs.py -n {wildcards.n} -l {wildcards.l} -o {wildcards.o} -m {wildcards.m} -i {wildcards.i} -d " + \
+		"{wildcards.d} > {output}"
+
+rule genRandSeqs:
+	output:
+		"../simulations/randSeqs_n{n}_l{l}_m{m}_i{i}_d{d}.ssv"
+	shell:
+		"python3 scripts/SimSeqPairs.py -n {wildcards.n} -l {wildcards.l} -m {wildcards.m} -i {wildcards.i} -d {wildcards.d} > {output}"
+
+rule constructSearchPairs:
+	input:
+		"../simulations/simGeneSeqs_n{gn}_l{gl}_o{o}_m{m}_i{i}_d{d}.ssv",
+		"../simulations/randSeqs_n{rn}_l{rl}_m{m}_i{i}_d{d}.ssv"
+	output:
+		"../simulations/searchPairs_gn{gn}_rn{rn}_gl{gl}_rl{rl}_o{o}_m{m}_i{i}_d{d}.txt"
+	run:
+		randSeqFile = open(input[1], 'r')
+		randSeqFile.readline()
+
+		for l in open(input[0], 'r'):
+			if not l.startswith("Random"):
+				genome = randSeqFile.readline().strip().split(' ')[0]
+				isPattern = True
+
+				for g in l.strip().split(' '):
+					if isPattern:
+						pattern = g
+						isPattern = False
+					else:
+						genome += g + randSeqFile.readline().strip().split(' ')[0]
+
+				shell("echo {pattern} {genome} >> {output}")
+
+rule searchHomologies:
+	input:
+		"../simulations/searchPairs_gn{gn}_rn{rn}_gl{gl}_rl{rl}_o{o}_m{m}_i{i}_d{d}.txt"
+	output:
+		"../simulations/homologies_gn{gn}_rn{rn}_gl{gl}_rl{rl}_o{o}_m{m}_i{i}_d{d}.txt"
+	run:
+		i = 0
+		for l in open(input[0], 'r'):
+			i += 1
+			shell("echo Pair {i} >> {output}")
+			l = l.strip()
+			pattern, text = l.split(' ')
+			shell("python3 scripts/FindThoms.py -p {pattern} -s {text} >> {output}")
--- scripts/SimSeqPairs.py
+++ scripts/SimSeqPairs.py
@@ -2,6 +2,7 @@
 
 import argparse as args
 from random import seed, choice, random, randrange
+from sys import maxsize
 
 #This script generates a given number of sequence pairs where one sequence is derived from the other after applying substitions and indels according 
 #to some rates.
@@ -13,20 +14,31 @@ NUCL_ALPHABET = "ACGT"
 SUB_BASES = {'A': "CGT", 'C': "AGT", 'G': "ACT", 'T': "ACG"}
 
 #Setting up the argument parser
-parser = args.ArgumentParser(description="This script generates a given number of random DNA sequences and mutates it resulting in a second sequence.")
-parser.add_argument('-s', metavar='Seed', type=int, default=42, help="The random seed to be used")
+parser = args.ArgumentParser(description="This script generates a given number of random DNA sequences and mutates them resulting in a given number of "\
+	+ "independent other sequences.")
+parser.add_argument('-s', metavar='Seed', type=int, help="The random seed to be used")
 parser.add_argument('-n', metavar='NbSeqs', type=int, required=True, help="Number of sequences to generate")
 parser.add_argument('-l', metavar='SeqLen', type=int, required=True, help="Length of sequences to generate")
 parser.add_argument('-m', metavar='SubsRate', type=float, default=0.1, help="Substitution rate")
 parser.add_argument('-i', metavar='InsRate', type=float, default=0.1, help="Insertion rate")
 parser.add_argument('-d', metavar='DelRate', type=float, default=0.1, help="Deletion rate")
+parser.add_argument('-o', metavar='MutNb', type=int, default=1, help="Number of (independent!) mutated sequences to generate")
 
 arguments = parser.parse_args()
 
-#Initialize with random seed
-seed(arguments.s)
+#Check if random seed is given
+if arguments.s:
+	sd = arguments.s
+else:
+	#Set randomly
+	sd = randrange(maxsize)
 
-#Generate sequence pairs
+#Initialize random seed
+seed(sd)
+
+print("Random seed used:", sd)
+
+#Generate sequences
 for i in range(arguments.n):
 	#Generate the first sequence
 	randomSeq = ""
@@ -34,31 +46,38 @@ for i in range(arguments.n):
 	for j in range(arguments.l):
 		randomSeq += choice(NUCL_ALPHABET)
 
-	#Mutate the sequence
-	mutatedSeq = randomSeq
+	#Output the sequence
+	print(randomSeq, end=' ')
 
-	for j in range(arguments.l):
-		#Check if a substitution takes place in this iteration
-		if random() < arguments.m:
-			#Decide for a position inside the sequence
-			pos = randrange(len(mutatedSeq))
-			#Introduce a substitution at a random position
-			mutatedSeq = mutatedSeq[:pos] + choice(SUB_BASES[mutatedSeq[pos]]) + mutatedSeq[pos + 1:]
+	#Mutate the sequence as often as needed
+	for i in range(arguments.o):
+		#Mutate the sequence
+		mutatedSeq = randomSeq
 
-		#Check if an insertion takes place in this iteration
-		if random() < arguments.i:
-			#Decide for a position inside the sequence
-			pos = randrange(len(mutatedSeq))
-			#Insert a random base and prune the sequence
-			mutatedSeq = mutatedSeq[:pos] + choice(NUCL_ALPHABET) + mutatedSeq[pos:len(mutatedSeq) - 1]
+		for j in range(arguments.l):
+			#Check if a substitution takes place in this iteration
+			if random() < arguments.m:
+				#Decide for a position inside the sequence
+				pos = randrange(len(mutatedSeq))
+				#Introduce a substitution at a random position
+				mutatedSeq = mutatedSeq[:pos] + choice(SUB_BASES[mutatedSeq[pos]]) + mutatedSeq[pos + 1:]
 
-		#Check if a deletion takes place in this iteration
-		if random() < arguments.d:
-			#Decide for a position inside the sequence
-			pos = randrange(len(mutatedSeq))
-			#Delete a base and append a random one
-			mutatedSeq = mutatedSeq[:pos] + mutatedSeq[pos + 1:] + choice(NUCL_ALPHABET)
+			#Check if an insertion takes place in this iteration
+			if random() < arguments.i:
+				#Decide for a position inside the sequence
+				pos = randrange(len(mutatedSeq))
+				#Insert a random base and prune the sequence
+				mutatedSeq = mutatedSeq[:pos] + choice(NUCL_ALPHABET) + mutatedSeq[pos:len(mutatedSeq) - 1]
 
-	#Output sequence pair
-	print(randomSeq, mutatedSeq)
+			#Check if a deletion takes place in this iteration
+			if random() < arguments.d:
+				#Decide for a position inside the sequence
+				pos = randrange(len(mutatedSeq))
+				#Delete a base and append a random one
+				mutatedSeq = mutatedSeq[:pos] + mutatedSeq[pos + 1:] + choice(NUCL_ALPHABET)
+
+		#Output mutated sequence
+		print(mutatedSeq, end=' ')
+
+	print("")
 		
