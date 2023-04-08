31acaba66022e006883b619107b94afc2884eca7
tischulz1 <tischulz@cebitec.uni-bielefeld.de>
2023-04-08 08:38:35 +0200

    Extended global edit distance calculation script to be able to handle multiple FASTA files and more analyses in notebooks

--- FindSimSeqs/CalcGlobEditDistance.cpp
+++ FindSimSeqs/CalcGlobEditDistance.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int main(int argc, char **argv){
-	parasail_sequence_t s1, s2;
+	// parasail_sequence_t s1, s2;
 	parasail_sequences_t *inSeqs1, *inSeqs2;
 	EdlibAlignResult result;
 
@@ -17,14 +17,32 @@ int main(int argc, char **argv){
 
 	//Load sequences
 	inSeqs1 = parasail_sequences_from_file(argv[1]);
-	s1 = inSeqs1->seqs[0];
 	inSeqs2 = parasail_sequences_from_file(argv[2]);
-	s2 = inSeqs2->seqs[0];
-	result = edlibAlign(s1.seq.s, s1.seq.l, s2.seq.s, s2.seq.l, edlibDefaultAlignConfig());
 
-	if (result.status == EDLIB_STATUS_OK) cout << result.editDistance << endl;
+	//Iterate over all pairs of sequences
+	for(uint32_t i = 0; i < inSeqs1->l; ++i){
+		parasail_sequence_t s1 = inSeqs1->seqs[i];
 
-	edlibFreeAlignResult(result);
+		//Testing
+		// cout << "Sequence 1: ";
+		// for(uint32_t k = 0; k < 10; ++k) cout << inSeqs1->seqs[i].seq.s[k];
+		// cout << endl;
+
+		for(uint32_t j = 0; j < inSeqs2->l; ++j){
+			parasail_sequence_t s2 = inSeqs2->seqs[j];
+
+			//Testing
+			// cout << "Sequence 2: ";
+			// for(uint32_t k = 0; k < 10; ++k) cout << inSeqs2->seqs[j].seq.s[k];
+			// cout << endl;
+
+			result = edlibAlign(s1.seq.s, s1.seq.l, s2.seq.s, s2.seq.l, edlibDefaultAlignConfig());
+			
+			if (result.status == EDLIB_STATUS_OK) cout << result.editDistance << endl;
+		
+			edlibFreeAlignResult(result);
+		}
+	}
 
 	return 0;
 }
