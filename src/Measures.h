1f6fbd1f1a8b90deb93d48838149bf626f3ed3ad
tischulz1 <tischulz@cebitec.uni-bielefeld.de>
2023-10-18 08:17:41 +0200

    Added function to calculate linear score using dynamically calculated x_min values

--- src/Measures.h
+++ src/Measures.h
@@ -11,4 +11,9 @@ const int32_t calcIntersecScore(const PairSketch& skA, const PairSketch& skB);
 //This function calculates the "maximum aligned hashes" similarity score
 const int32_t calcAlgnHshsScore(const PairSketch& skA, const PairSketch& skB, const bool& consOffs);
 
+//This function calculates the linear score
+inline const float calcLinScore(const uint32_t& xmin, const uint32_t& pLen, const uint32_t& i, const uint32_t& j, const float& uw){//TODO: This function still needs to be tested!
+	return xmin - w * (max(pLen, j - i + 1) - xmin);
+}
+
 #endif
