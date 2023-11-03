242bcb669980ea0f19ff02b765786002df72ec6a
tischulz1 <tischulz@cebitec.uni-bielefeld.de>
2023-11-03 09:49:39 +0100

    Removed some commented code

--- src/Measures.h
+++ src/Measures.h
@@ -13,11 +13,6 @@ const int32_t calcAlgnHshsScore(const PairSketch& skA, const PairSketch& skB, co
 
 //This function calculates the linear score
 inline const float calcLinScore(const int32_t& xmin, const int32_t& pLen, const int32_t& i, const int32_t& j, const float& w){//TODO: This function still needs to be tested!
-	//Testing
-	// if(i == 2501333 && j == 2503033) cout << "calcLinScore: xmin: " << xmin << " w: " << w << " pLen: " << pLen << " L[i]: " << i << " L[j]: " << j << endl;
-	// cout << "calcLinScore: xmin: " << "L[j] - L[i]: " << j - i << endl;
-	// cout << "calcLinScore: max(pLen, L[j] - L[i] + 1) - xmin: " << max(pLen, j - i + 1) - xmin << endl;
-
 	return xmin - w * ((pLen + (j - i + 1)) - 2 * xmin);
 }
 
--- src/Thomology.cpp
+++ src/Thomology.cpp
@@ -41,10 +41,6 @@ void findThoms(const Sketch& skP, const mm_idx_t *tidx, const uint32_t& cw, cons
 	//Initialize hloc (again, we assume there are no duplicates in t)
 	unordered_map<uint64_t, uint32_t> hloc(skP.size());
 
-	//Testing
-	// cout << "maxScores.size(): " << maxScores.size() << endl;
-	// exit(0);
-
 	//We do not support the exclusion of nested results currently
 	if(noNesting){
 		cerr << "ERROR: No nesting is currently not supported!" << endl;
@@ -65,10 +61,6 @@ void findThoms(const Sketch& skP, const mm_idx_t *tidx, const uint32_t& cw, cons
 	//Reserve enough space for x_min vector
 	xmin.reserve(L.size());
 
-	//Testing
-	// cout << "findThoms: L.size(): " << L.size() << endl;
-	// return;
-
 	//Base case: Deal with candidate mappings ending with last k-mer in L//
 
 	//Set j to index of last k-mer in L
@@ -85,19 +77,8 @@ void findThoms(const Sketch& skP, const mm_idx_t *tidx, const uint32_t& cw, cons
 	//x_min is 1 for the candidate mapping only containing the last k-mer of L by definition
 	xmin.push_back(1);
 
-	//Testing
-	// fKmIt = L.rbegin();
-	// ++fKmIt;
-	// cout << "findThoms: fKmIt->second (using rbegin() + ++): " << fKmIt->second << endl;
-	// uint32_t itCounter = 0;
-
 	//Iterate over start positions in reverse order
 	for(fKmIt = L.rbegin() + 1; fKmIt != L.rend(); ++fKmIt){//, ++itCounter
-		//Testing
-		// cout << "findThoms: fKmIt->second (using rbegin() + 1): " << fKmIt->second << endl;
-		// if(fKmIt->first == 83349362) cout << "Found k-mer at position " << fKmIt->second << endl;
-		// exit(0);
-
 		//Fill hloc once iterating over L for the first time
 		if(hloc.contains(fKmIt->first)){
 			++hloc[fKmIt->first];
@@ -119,21 +100,10 @@ void findThoms(const Sketch& skP, const mm_idx_t *tidx, const uint32_t& cw, cons
 		lstRrsnbl -= (isRr ? 1 : 0);
 	}
 
-	//Testing
-	// cout << "Filled first column of xmin" << endl;
-
 	//Find final mappings in last column
 	for(i = 0; i < L.size(); ++i){
-		//Testing
-		// cout << "i: " << i << endl;
-
 		//Calculate linear score for current mapping
 		currScr = calcLinScore(xmin[i], skP.size(), L[i].second, L[j].second, uw);
-
-		//Testing
-		// cout << "lstRrsnbl: " << lstRrsnbl << endl;
-		// cout << "currScr: " << currScr << endl;
-		// cout << "maxThres: " << maxThres << endl;
 
 		//Check if mapping is final
 		if(currScr > maxThres && lstRrsnbl <= i){
@@ -150,26 +120,6 @@ void findThoms(const Sketch& skP, const mm_idx_t *tidx, const uint32_t& cw, cons
 		}
 	}
 
-	//Testing
-	// cout << "First column done" << endl;
-	// cout << "Number of iterations to iterate over it: " << itCounter << endl;
-	// cout << "res.size(): " << res.size() << endl;
-	// cout << "maxThres: " << maxThres << endl;
-	// cout << "xmin.size(): " << xmin.size() << endl;
-	// for(uint32_t k = 0; k < 10; ++k){
-	// 	cout << "xmin[" << k << "]: " << xmin[k] << endl;
-	// }
-	// // for(uint32_t k = 0; k < 10; ++k){
-	// // 	cout << "L[" << k << "]: " << L[k].first << " " << L[k].second << endl;
-	// // }
-	// cout << "xmin[L.size()-2]: " << xmin[L.size()-2] << endl;
-	// cout << "xmin[L.size()-1]: " << xmin[L.size()-1] << endl;
-	// cout << "L[L.size()-2]: " << L[L.size()-2].first << " " << L[L.size()-2].second << endl;
-	// cout << "L[L.size()-1]: " << L[L.size()-1].first << " " << L[L.size()-1].second << endl;
-	// exit(0);
-	// cout << "maxScores.size(): " << maxScores.size() << endl;
-	// exit(0);
-
 	//Iterate over all reasonable candidate mappings
 	for(--j, jPosKmIt = L.rbegin() + 1; jPosKmIt != L.rend(); ++lKmIt, --j, ++jPosKmIt){
 		//Initalize counter to count how many times the last k-mer of the previous candidate mapping has been seen while iterating 
@@ -183,33 +133,12 @@ void findThoms(const Sketch& skP, const mm_idx_t *tidx, const uint32_t& cw, cons
 		//Update hloc
 		--hloc[lKmIt->first];
 
-		//Testing
-		// int32_t xminDecCount = xmin[i];
-		// if(L[j].second == 2503034){
-		// 	cout << "lstRrsnbl (beginning of this column): " << lstRrsnbl << endl;
-		// 	// cout << "hloc[jPosKmIt->first]: " << hloc[jPosKmIt->first] << endl;
-		// 	// cout << "occp[jPosKmIt->first]: " << occp[jPosKmIt->first] << endl;
-		// 	cout << "L[3484].first: " << L[3484].first << endl;
-		// 	cout << "L[3485].first: " << L[3485].first << endl;
-		// }
-
 		//Iterate over start positions in non-reverse order
 		for(fLit = L.begin(), i = 0; i <= j; ++fLit, ++i){
-			//Testing
-			// if(L[j].second == 2503034 && fLit->first == 83349362){
-			// 	cout << "i: " << i << endl;
-			// 	cout << "hloc[jPosKmIt->first]: " << hloc[jPosKmIt->first] << endl;
-			// 	cout << "occPosJ: " << occPosJ << endl;
-			// 	cout << "occp[jPosKmIt->first]: " << occp[jPosKmIt->first] << endl;
-			// }
-
 			//Check right reasonability considering the previously last k-mer
 			if(hloc[lKmIt->first] + 1 - occ <= occp[lKmIt->first]){
 				//Decrement x_min
 				xmin[i] -= 1;
-
-				//Testing
-				// --xminDecCount;
 			}
 
 			//Check right reasonability considering the currently last k-mer
@@ -226,45 +155,12 @@ void findThoms(const Sketch& skP, const mm_idx_t *tidx, const uint32_t& cw, cons
 			//occPosJ changes for the next iteration of the first and the currently last k-mer of the candidate mapping have an 
 			//identical sequence
 			occPosJ += (fLit->first == jPosKmIt->first ? 1 : 0);
-
-			//Testing
-			// if(occ > hloc[lKmIt->first]){
-			// 	cout << "occ > hloc[lKmIt->first]; this should never happen!" << endl;
-			// 	cout << "occ: " << occ << endl;
-			// 	cout << "hloc[" << lKmIt->first << "]: " << hloc[lKmIt->first] << endl;
-			// 	cout << "i: " << i << endl;
-			// 	cout << "j: " << j << endl;
-			// 	// exit(0);
-			// }
 		}
-
-		//Testing
-		// if(xminDecCount < 0){
-		// 	cout << "We decreased xmin too much" << endl;
-		// cout << "i: " << i << " j: " << j << endl;
-		// cout << "maxScores.size(): " << maxScores.size() << endl;
-		// exit(0);
-		// }
 		
 		//Iterate over start positions again
 		for(fLit = L.begin(), i = 0, li = maxScores.begin(), maxThres = t - 1; i <= j; ++fLit, ++i){
-			//Testing
-			// if(L[i].second == 503756 && L[j].second == 504057){
-			// 	cout << "i: " << i << endl;
-			// 	cout << "maxThres: " << maxThres << endl;
-			// 	cout << "li->first: " << li->first << endl;
-			// 	cout << "maxScores.size(): " << maxScores.size() << endl;
-			// 	cout << "li != maxScores.end(): " << (li != maxScores.end() ? "True" : "False") << endl;
-			// 	li = maxScores.begin();
-			// 	cout << "li != maxScores.end(): " << (li != maxScores.end() ? "True" : "False") << endl;
-			// 	// exit(0);
-			// }
-
 			//Update threshold in case we have seen a relevant final mapping already
 			if(li != maxScores.end() && li->first == i){
-				//Testing
-				// if(L[i].second == 503756 && L[j].second == 504057) cout << "Update score" << endl;
-
 				maxThres = max(li->second, maxThres);
 				//In memory of the "smooth iterator"
 			}
@@ -272,82 +168,19 @@ void findThoms(const Sketch& skP, const mm_idx_t *tidx, const uint32_t& cw, cons
 			//Calculate linear score
 			currScr = calcLinScore(xmin[i], skP.size(), L[i].second, L[j].second, uw);
 
-			//Testing
-			// if(L[i].second == 503756 && L[j].second == 504057){
-			// 	cout << "i: " << i << " j: " << j << endl;
-			// 	// cout << "currScr: " << currScr << endl;
-			// 	cout << "maxThres: " << maxThres << endl;
-			// 	// cout << "lstRrsnbl: " << lstRrsnbl << endl;
-			// 	// cout << "xmin[i]: " << xmin[i] << endl;
-			// 	// cout << "xmin[i+1]: " << xmin[i+1] << endl;
-			// 	// uint32_t lKmCnt = 0;
-			// 	// for(uint32_t k = 6171; k < 9223; ++k){
-			// 	// 	if(L[k].first == L[9222].first) ++lKmCnt;
-			// 	// }
-			// 	// cout << "Abundance of last k-mer in interval: " << lKmCnt << endl;
-			// 	// cout << "Abundance of last k-mer in pattern: " << occp[L[9222].first] << endl;
-			// 	// cout << "L[1848].first: " << L[1848].first << endl;
-			// 	// cout << "L[3483].first: " << L[3483].first << endl;
-			// }
-
 			//Check if candidate mapping is final
 			if(currScr > maxThres && i >= lstRrsnbl && (i == j || xmin[i] == xmin[i + 1] + 1)){
-				//Testing
-				// cout << "Final mapping found" << endl;
-				// cout << "currScr: " << currScr << endl;
-				// cout << "maxThres: " << maxThres << endl;
-				// cout << "findThoms: i: " << i << " j: " << j << endl;
-				// cout << "findThoms: xmin[i]: " << xmin[i] << endl;
-				// exit(0);
-				// if(L[i].second == 3573864 && L[j].second == 3575170){
-				// 	cout << "currScr: " << currScr << endl;
-				// 	cout << "maxThres: " << maxThres << endl;
-				// 	cout << "L[i].first: " << L[i].first << endl;
-				// 	cout << "i: " << i << " j: " << j << endl;
-				// 	cout << "First hash in pattern sketch: " << skP.front() << endl;
-				// 	cout << "skP.size(): " << skP.size() << endl;
-				// 	exit(0);
-				// }
-
 				//Add mapping to result list
 				res.push_back(make_tuple(L[i].second, L[j].second, currScr));
 
 				//Keep score for comparison to other scores of remaining candidate mappings starting with the same k-mer or later
 				if(li != maxScores.end() && i == li->first){
-					//Testing
-					// if(L[i].second == 503756 && L[j].second == 504058){
-					// 	cout << "maxScores.size(): " << maxScores.size() << endl;
-					// 	cout << "Do we insert an element inside an empty list???" << endl;
-					// }
-
 					//Override existing maximum for candidate mapping starting with current k-mer
 					*li = make_pair(i, currScr);
-
-					//Testing
-					// if(L[i].second == 503756 && L[j].second == 504058){
-					// 	cout << "maxScores.size(): " << maxScores.size() << endl;
-					// }
 				} else{
-					//Testing
-					// if(L[i].second == 503756 && L[j].second == 504058){
-					// 	cout << "maxScores.size(): " << maxScores.size() << endl;
-					// }
-
 					//Add a new entry
 					maxScores.insert(li, make_pair(i, currScr));
-
-					//Testing
-					// if(L[i].second == 503756 && L[j].second == 504058){
-					// 	cout << "maxScores.size() (after insert): " << maxScores.size() << endl;
-					// }
 				}
-
-				//Testing
-				// if(L[i].second == 3573864 && L[j].second == 3575171){
-				// 	cout << "Here we should have updated the score:" << endl;
-				// 	cout << "currScr: " << currScr << endl;
-				// 	cout << "li->second: " << li->second << endl;
-				// }
 
 				//Update score threshold 
 				maxThres = currScr;
@@ -362,20 +195,7 @@ void findThoms(const Sketch& skP, const mm_idx_t *tidx, const uint32_t& cw, cons
 			outputHoms(res, normalize, skP.size());
 			res.clear();
 		}
-
-		//Testing
-		// if(j == L.size()-1000){
-		// 	cout << "Processing of column " << j << " done" << endl;
-		// 	return;
-		// }
 	}
 
-	//Testing
-	// cout << "Outputting results...res: " << endl;
-	// cout << "res.size(): " << res.size() << endl;
-
 	outputHoms(res, normalize, skP.size());
-
-	//Testing
-	// cout << "Read processed" << endl;
 }
